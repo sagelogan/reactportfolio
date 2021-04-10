284a1cbb8db6f7197b506ad1202aa98fd45a3eb6
Logan Sage <lvdsage@gmail.com>
2021-04-09 18:36:38 -0600

    trying to fix things

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
--- portfolio/src/components/Header/index.js
+++ portfolio/src/components/Header/index.js
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar,Row, Container, Col, Jumbotron} from 'react-bootstrap';
+import { Navbar,Row, Container, Col, Jumbotron, Button} from 'react-bootstrap';
 import './Header.css';
 
 function Header() {
 
   return (
     <>
-      <Jumbotron fixed="top" className="justify-content-center text-align-center" >
-            <Container className="justify-content-center">
-              <Row>
-                <Col>
-                <a id="logo-container" href="#" class="brand-logo">Logan Sage</a>
-                </Col>
-              </Row>
-            </Container>
+      <Container class="header">
+        <Jumbotron>
+          <h1>Logan Sage</h1>
         </Jumbotron>
+      </Container>
     </>
   );
 }
